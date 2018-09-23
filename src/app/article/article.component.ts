2a90b781579cc91408e6158824066a87665e810e
Victor Alonso <vacontrerasmeza@gmail.com>
2018-09-23 13:30:47 -0500

    voteUp and voteDown fuctions added

--- src/app/article/article.component.ts
+++ src/app/article/article.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostBinding } from '@angular/core';
 
 @Component({
   selector: 'app-article',
@@ -6,10 +6,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./article.component.css']
 })
 export class ArticleComponent implements OnInit {
+  @HostBinding('attr.class') cssClass = 'row'; // This adds an attribute to the enterer app-article tag.
+  votes: number;
+  title: string;
+  link: string;
 
-  constructor() { }
+  constructor() {
+    this.title = 'Angular 2';
+    this.link = 'http://angular.io';
+    this.votes = 10;
+   }
+
+   voteUp(): boolean {
+     this.votes += 1;
+     return false; // JavaScript, by default, propagates the click event to all the parent components.
+   }
+
+   voteDown(): boolean {
+     this.votes -= 1;
+     return false; // To fix that, we need to make the click event handler to return false.
+   }
 
   ngOnInit() {
   }
+
 
 }
